f8a300b29aab5b42aa0bfe5d6b22f9be5312b182
Rob Slagter <rob@hedude.com>
2019-08-05 14:30:13 +0200

    Added editor

---
+++ editor/data/active.php
@@ -0,0 +1,3 @@
+<?php
+/*|[{"username":"admin","path":"code\/index.cgi","focused":true}]|*/
+?>
---
+++ editor/data/users.php
@@ -0,0 +1,3 @@
+<?php
+/*|[{"username":"admin","password":"4643dd198f9fb8ab7ac0f794527358157a53aab3","project":"code"}]|*/
+?>
