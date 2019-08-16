a05d62ad99b2dbbb7662e19396d3f5522991263e
bijbrengen <slagter@bijbrengen.nl>
2019-08-16 11:48:13 +0200

    Added login form for hacking practice

--- hack/mypage.php
+++ hack/mypage.php
@@ -1,3 +1,7 @@
-<?php
-$passwords = file_get_contents( 'passwords.txt' );
-echo $passwords;
+<html>
+<body>
+    <pre>&lt;?php</pre>
+    <pre>$passwords = file_get_contents( 'passwords.txt' );</pre>
+    <pre>echo $passwords;</pre>
+</body>
+</html>
