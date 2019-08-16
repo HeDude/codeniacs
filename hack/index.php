2952f6716d405d47d507f284ce7564b5c409b0fb
bijbrengen <slagter@bijbrengen.nl>
2019-08-16 10:41:51 +0200

    Added login form for hacking practice

--- hack/index.php
+++ hack/index.php
@@ -2,8 +2,8 @@
 <head>
     <title>Login</title>
 </head>
-    <h3>Add entry</h3>
-    <p> Add another Article</p>
+    <h3>Login system administrator</h3>
+    <p>Pas op dat niemand je meekijkt als je je gebruikersnaam en wachtwoord invult</p>
     <form action="entry.php" method = "post">
         <label for="username">Username</label> <input type="username" id="usename" name="username"><br /><br />
         <label for="password">Password:</label> <input type="text" id="password" name="password"><br /><br />
