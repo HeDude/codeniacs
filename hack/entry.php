6d368f79017da7f0eb8a9e3efe040ce859174dbf
bijbrengen <slagter@bijbrengen.nl>
2019-08-16 10:40:10 +0200

    Added login form for hacking practice

---
+++ hack/entry.php
@@ -0,0 +1,34 @@
+<html>
+    <title>Login</title>
+    <body>
+        <?php
+        $lines = file( "passwords.txt", FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES );
+        $passwords = [];
+
+        for( $i = 0; $i < count( $lines ) - 1; $i++ )
+        {
+            $passwords[ $lines[ $i ] ] = $lines[ $i + 1 ];
+        }
+
+        $username = htmlspecialchars( $_POST['username'] );
+        if
+        (
+            array_key_exists( $username, $passwords )
+            &&
+            $passwords[ $username ] == $_POST['password']
+        )
+        {
+            echo "je hebt me gehackt";
+
+        }
+        else
+        {
+            echo "incorrect login";
+        }
+        ?>
+
+        <form name="input" action="index.php" method="get">
+            <input type="submit" value="Home">
+        </form>
+    </body>
+</html>
